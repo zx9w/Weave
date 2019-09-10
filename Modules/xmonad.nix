8fd9bfc1ade44a1a37108b5b9f60e357c9bc659b
zx9w <ilmu@rishi.is>
2019-09-10 22:32:43 +0200

    Some plans and rearrangement

---
+++ Modules/xmonad.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+pkgs.writeHaskellPackage "xmonad-ilmu" {
+  executable.xmonad = {
+    extra-depends = [
+      "containers"
+      "extra"
+      "unix"
+      "X11"
+      "xmonad"
+      "xmonad-contrib"
+      "xmonad-extra"
+      "xmonad-zone"
+    ];
+    text = builtins.readFile ./xmonad.hs;
+  };
+}
